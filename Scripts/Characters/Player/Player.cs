84895a7d9a4e125e96e6dbcb33d3682a537ad020
aniran <aniran530@gmail.com>
2025-03-11 17:15:05 +0530

    Implement player movement and input handling; update main scene with environment settings

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -3,13 +3,30 @@ using System;
 
 public partial class Player : CharacterBody3D
 {
-	// Called when the node enters the scene tree for the first time.
+	// variables
+	private Vector2 _inputDirection = new();
+	
+	//exported variables
+	[Export]
+	private float _speed = 5.0f;
+	
 	public override void _Ready()
 	{
 	}
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 	}
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Velocity = new( _inputDirection.X, 0, _inputDirection.Y );
+		Velocity *= _speed;
+		MoveAndSlide();
+    }
+
+    public override void _Input(InputEvent @event)
+    {
+		_inputDirection = Input.GetVector("MoveLeft", "MoveRight", "MoveForward", "MoveBack");       
+    }
 }
